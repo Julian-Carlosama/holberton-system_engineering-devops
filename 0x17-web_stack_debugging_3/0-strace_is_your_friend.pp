39db371e61471bc17b033187870894b214ea7ab6
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-02-16 23:40:34 -0500

    Add commits for this task

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Fix 500 error when a GET HTTP method is requested to Apache web server
 exec { 'fix':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
